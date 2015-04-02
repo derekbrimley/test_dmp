fb4153bef474923e3fa39fa68870d00ac27bcd4b
Derek <derekbrimley@gmail.com>
2015-04-02 14:10:42 -0600

    added 2 seo features

--- homepage/cached_templates/templates/base_app/templates/base.htm.py
+++ homepage/cached_templates/templates/base_app/templates/base.htm.py
@@ -4,13 +4,13 @@ UNDEFINED = runtime.UNDEFINED
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1427522027.912939
+_modified_time = 1428005043.979372
 _enable_loop = True
 _template_filename = 'C:\\Users\\Derek\\python\\test_dmp/base_app/templates/base.htm'
 _template_uri = '/base_app/templates/base.htm'
 _source_encoding = 'ascii'
 import os, os.path, re
-_exports = ['content', 'nav', 'account', 'header']
+_exports = ['content', 'account', 'header', 'nav']
 
 
 from django_mako_plus.controller import static_files 
@@ -19,17 +19,17 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        self = context.get('self', UNDEFINED)
-        STATIC_URL = context.get('STATIC_URL', UNDEFINED)
         def content():
             return render_content(context._locals(__M_locals))
+        self = context.get('self', UNDEFINED)
         def account():
             return render_account(context._locals(__M_locals))
+        STATIC_URL = context.get('STATIC_URL', UNDEFINED)
+        def header():
+            return render_header(context._locals(__M_locals))
         def nav():
             return render_nav(context._locals(__M_locals))
         request = context.get('request', UNDEFINED)
-        def header():
-            return render_header(context._locals(__M_locals))
         __M_writer = context.writer()
         __M_writer('\r\n')
         __M_writer('\r\n')
@@ -37,7 +37,7 @@ def render_body(context,**pageargs):
         
         __M_locals_builtin_stored = __M_locals_builtin()
         __M_locals.update(__M_dict_builtin([(__M_key, __M_locals_builtin_stored[__M_key]) for __M_key in ['static_renderer'] if __M_key in __M_locals_builtin_stored]))
-        __M_writer('\r\n\r\n<!DOCTYPE html>\r\n<html>\r\n\t<meta charset="UTF-8">\r\n\t<head>\r\n\r\n\t<title>homepage</title>\r\n\r\n')
+        __M_writer('\r\n\r\n<!DOCTYPE html>\r\n<html>\r\n\t<meta charset="UTF-8">\r\n\t<head>\r\n\r\n\t<title>Colonial Heritage Foundation</title>\r\n\t<meta name="description" content="The Colonial Heritage Foundation organizes events happening in Utah that celebrate the Revolutionary time period, and offers period products for purchase or rental.">\r\n')
         __M_writer('\t<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>\r\n\t<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">\r\n\t<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>\r\n\t<link rel="icon" type="image/x-icon" href="\\static\\homepage\\media\\favicon.ico" />\r\n\t<link href=\'http://fonts.googleapis.com/css?family=Roboto:100\' rel=\'stylesheet\' type=\'text/css\'>\r\n\t<script src="')
         __M_writer(str(STATIC_URL))
         __M_writer('base_app/media/jquery.loadmodal.js"></script>\r\n\t<script src="')
@@ -85,24 +85,12 @@ def render_content(context,**pageargs):
         context.caller_stack._pop_frame()
 
 
-def render_nav(context,**pageargs):
-    __M_caller = context.caller_stack._push_frame()
-    try:
-        def nav():
-            return render_nav(context)
-        __M_writer = context.writer()
-        __M_writer('\r\n\t\t\t\t\t\t\tNAVIGATION BAR\r\n\t\t\t\t\t\t')
-        return ''
-    finally:
-        context.caller_stack._pop_frame()
-
-
 def render_account(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        request = context.get('request', UNDEFINED)
         def account():
             return render_account(context)
+        request = context.get('request', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\r\n')
         if request.user.is_authenticated():
@@ -118,9 +106,9 @@ def render_account(context,**pageargs):
 def render_header(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        request = context.get('request', UNDEFINED)
         def header():
             return render_header(context)
+        request = context.get('request', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\r\n\t\t\t\t\tThe Colonial Heritage Foundation\r\n')
         if request.user.is_authenticated():
@@ -133,8 +121,20 @@ def render_header(context,**pageargs):
         context.caller_stack._pop_frame()
 
 
+def render_nav(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        def nav():
+            return render_nav(context)
+        __M_writer = context.writer()
+        __M_writer('\r\n\t\t\t\t\t\t\tNAVIGATION BAR\r\n\t\t\t\t\t\t')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
 """
 __M_BEGIN_METADATA
-{"filename": "C:\\Users\\Derek\\python\\test_dmp/base_app/templates/base.htm", "line_map": {"128": 32, "129": 32, "130": 34, "107": 48, "68": 66, "69": 75, "70": 75, "136": 130, "108": 49, "82": 44, "76": 44, "109": 50, "16": 4, "18": 0, "110": 60, "88": 39, "100": 48, "111": 61, "94": 39, "112": 66, "34": 2, "35": 4, "36": 5, "40": 5, "41": 15, "42": 20, "43": 20, "44": 21, "45": 21, "46": 23, "47": 23, "48": 23, "53": 34, "118": 29, "58": 41, "127": 32, "125": 29, "126": 31, "63": 45}, "uri": "/base_app/templates/base.htm", "source_encoding": "ascii"}
+{"line_map": {"130": 39, "68": 66, "69": 75, "70": 75, "136": 130, "82": 44, "76": 44, "98": 60, "16": 4, "124": 39, "18": 0, "99": 61, "88": 48, "100": 66, "95": 48, "96": 49, "97": 50, "34": 2, "35": 4, "36": 5, "40": 5, "41": 15, "42": 20, "43": 20, "44": 21, "45": 21, "46": 23, "47": 23, "48": 23, "113": 29, "114": 31, "115": 32, "116": 32, "53": 34, "118": 34, "58": 41, "117": 32, "106": 29, "63": 45}, "uri": "/base_app/templates/base.htm", "source_encoding": "ascii", "filename": "C:\\Users\\Derek\\python\\test_dmp/base_app/templates/base.htm"}
 __M_END_METADATA
 """
