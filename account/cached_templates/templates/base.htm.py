8f8b4845706a9ce0517998d2bb2b873cccfa5080
Derek <derekbrimley@gmail.com>
2015-04-06 09:05:54 -0600

    other changes

--- account/cached_templates/templates/base.htm.py
+++ account/cached_templates/templates/base.htm.py
@@ -4,13 +4,13 @@ UNDEFINED = runtime.UNDEFINED
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1427771871.354739
+_modified_time = 1428332411.090011
 _enable_loop = True
 _template_filename = 'C:\\Users\\Derek\\python\\test_dmp\\account\\templates/base.htm'
 _template_uri = 'base.htm'
 _source_encoding = 'ascii'
 import os, os.path, re
-_exports = ['nav']
+_exports = ['title', 'nav']
 
 
 def _mako_get_namespace(context, name):
@@ -29,14 +29,33 @@ def render_body(context,**pageargs):
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
         request = context.get('request', UNDEFINED)
+        def title():
+            return render_title(context._locals(__M_locals))
         def nav():
             return render_nav(context._locals(__M_locals))
         __M_writer = context.writer()
-        __M_writer('\r\n\r\n')
+        __M_writer('\r\n')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'title'):
+            context['self'].title(**pageargs)
+        
+
+        __M_writer('\r\n')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'nav'):
             context['self'].nav(**pageargs)
         
 
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_title(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        def title():
+            return render_title(context)
+        __M_writer = context.writer()
+        __M_writer('\r\n\t<title>Account</title>\r\n')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -60,6 +79,6 @@ def render_nav(context,**pageargs):
 
 """
 __M_BEGIN_METADATA
-{"filename": "C:\\Users\\Derek\\python\\test_dmp\\account\\templates/base.htm", "uri": "base.htm", "source_encoding": "ascii", "line_map": {"35": 1, "52": 3, "53": 16, "54": 17, "55": 33, "27": 0, "45": 3, "61": 55}}
+{"uri": "base.htm", "line_map": {"64": 5, "80": 74, "74": 35, "52": 2, "37": 1, "71": 5, "72": 18, "73": 19, "58": 2, "27": 0, "42": 4}, "source_encoding": "ascii", "filename": "C:\\Users\\Derek\\python\\test_dmp\\account\\templates/base.htm"}
 __M_END_METADATA
 """
